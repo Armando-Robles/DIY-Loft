d824cde5b312feb152f91d12478a374f57a67bd6
Armando Robles <mrspinosausurs1110@gmail.com>
2023-07-01 23:48:01 -0600

    Added/edited content to the pages

--- src/views/King.vue
+++ src/views/King.vue
@@ -1,87 +1,103 @@
 <script setup>
-import Steps from '../components/Steps.vue'
+import Steps from "../components/Steps.vue";
+import Lsh from "../components/Pageheader.vue";
+import Otp from "../components/Onthispage.vue";
+import Mn from "../components/Materials.vue";
+import Pricing from '../components/Pricing.vue';
 </script>
 
 <template>
-  
+  <div id="about"> <!-- Loft Size Header -->
+    <Lsh>
+      <template #lsh> Long live the king!</template>
+      <template #lshp>
+        Unlike Mufasa, you won't be going down.
+        <div>
+          <img src="../assets/lltk.png" alt="Long live the King"/>
+        </div>
+      
+      </template>
+    </Lsh>
+  </div>
 
+  <div> <!-- On This Page -->
+    <Otp>
+      <template #otph>
+        Here is a breakdown of what you'll find on this page!
+      </template>
+      <template #otpp>
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#materials">Materials</a></li>
+          <li><a href="#pricing">Pricing</a></li>
+          <li><a href="#steps">Steps</a></li>
+          <li><a href="#extras">Extras</a></li>
+        </ul>
+      </template>
+    </Otp>
+  </div>
 
-  <Steps>
-    <template #icon>
-      1
-    </template>
-    <template #heading>Step 1</template>
+  <div id="materials"> <!-- Materials -->
+    <Mn>
+      <template #mnh>
+        Here is a list of the materials that will be needed for this project.
+      </template>
+      <template #mnp>
+        <ul>
+          <li>Wood</li>
+          <li>Tools</li>
+          <li>Determination</li>
+          <li>Courage</li>
+          <li>A nice lunch</li>
+        </ul>
+      </template>
+    </Mn>
+  </div>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </Steps>
+  <div id="pricing"> <!-- Pricing -->
+    <Pricing>
+      <template #ph>
+        What should this project cost me?
+      </template>
+      <template #pp>
+        Let's talk about that..
+      </template>
+    </Pricing>
+  </div>
 
-  <Steps>
-    <template #icon>
-      2
-    </template>
-    <template #heading>Step 2</template>
+  <div id="steps"> <!-- Steps portion -->
+    <Steps>
+      <template #icon> 1 </template>
+      <template #heading>Step 1</template>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, ex.
+     </Steps>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
+    <Steps>
+      <template #icon> 2 </template>
+      <template #heading>Step 2</template>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, ex.
+    </Steps>
 
-    <br />
+    <Steps>
+      <template #icon> 3 </template>
+      <template #heading>Step 3</template>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, ex.a
+    </Steps>
 
-    More instructions are available in <code>README.md</code>.
-  </Steps>
+    <Steps>
+      <template #icon> 4 </template>
+      <template #heading>Step 4</template>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, ex.
+    </Steps>
 
-  <Steps>
-    <template #icon>
-      3
-    </template>
-    <template #heading>Step 3</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </Steps>
-
-  <Steps>
-    <template #icon>
-      4
-    </template>
-    <template #heading>Step 4</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </Steps>
-
-  <Steps>
-    <template #icon>
-      5
-    </template>
-    <template #heading>Step 5</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </Steps>
+    <Steps>
+      <template #icon> 5 </template>
+      <template #heading>Step 5</template>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, ex.
+    </Steps>
+  </div>
 </template>
+
 
 <style>
 @media (min-width: 1024px) {
