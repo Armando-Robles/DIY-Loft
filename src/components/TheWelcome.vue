3f772ddc40a46fb0385ce5508f6ff3eb81cbe48e
Armando Robles <mrspinosausurs1110@gmail.com>
2023-07-16 22:39:13 -0600

    Iframe upload and css

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -9,13 +9,26 @@ import Steps from '../components/Steps.vue'
     websites that are similar to this one, but there's one big difference. We won't make you pay us for the plans! Now if you'd like to leave a tip we
   would very much appreciate it, but its not required! </p>
 </div>
-<div> 
-  <iframe width="1691" height="779" src="https://www.youtube.com/embed/5eyxo8kiPiU" title="DIY Kids Floating Loft Bed  | TimberLOK Lag Screws" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<div class="desc"> 
+  <p>This is the video that first gave me the idea to build a loft bed! I had to make some ajustments because there is a size difference between a twin and a queen. 
+  </p>
+  <iframe width="700" height="350" src="https://www.youtube.com/embed/5eyxo8kiPiU" title="DIY Kids Floating Loft Bed  | TimberLOK Lag Screws" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </div>
 </template>
 
 <style>
 .intro {
   align-items: center;
+  padding: 15px;
+}
+.desc{
+  align-items: center;
+  padding: 15px
+}
+.desc p{
+  padding: 10px;
+}
+.desc iframe{
+  margin-left: 20%;
 }
 </style>
